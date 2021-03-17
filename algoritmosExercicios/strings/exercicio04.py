ad1c5975a42b552f2424d35c03d717393c2ed81d
Thiago Ferreira <thiagofb84jp@gmail.com>
2021-03-17 09:40:05 -0300

    Including a new project based on Python book.

---
+++ algoritmosExercicios/strings/exercicio04.py
@@ -0,0 +1,6 @@
+'''
+04. Write a Python program to get a string from a given string where
+    all occurrences of its first char have been changed to '$', except
+    the first char itself. 
+'''
+
---
+++ pythonBook/chapterOne/teste.py
@@ -0,0 +1,4 @@
+print("Oi")
+print("Hello World")
+print("I love to program some language.")
+print("I love to code and I will code every day.")
