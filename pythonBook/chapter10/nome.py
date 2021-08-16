17b8d04d477aeccf29a8ab5d61b367f6b3897b95
Thiago Barbosa <thiago_barbosa@apple.com>
2021-08-16 17:14:24 -0300

    Including more exercises about Python book

--- pythonBook/chapter10/nome.py
+++ pythonBook/chapter10/nome.py
@@ -1,4 +1,4 @@
-# Verifica o nome do usuário
+# Chave como propriedade apenas para leitura
 
 from functools import total_ordering
 
@@ -6,16 +6,13 @@ from functools import total_ordering
 @total_ordering
 class Nome:
     def __init__(self, nome):
-        if nome is None or not nome.strip():
-            raise ValueError("Nome não pode ser nulo nem em branco.")
         self.nome = nome
-        self.chave = nome.strip().lower()
 
     def __str__(self):
         return self.nome
 
     def __repr__(self):
-        return f"<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.nome} Chave: {self.chave}>"
+        return f"<Classe {type(self).__name__} em 0x{id(self):x} Nome: {self.__nome} Chave: {self.__chave}>"
 
     def __eq__(self, outro):
         print("__eq__ Chamado")
@@ -24,6 +21,21 @@ class Nome:
     def __lt__(self, outro):
         print("__lt__ Chamado")
         return self.nome < outro.nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, valor):
+        if valor is None or not valor.strip():
+            raise ValueError("Nome não pode ser nulo nem em branco")
+        self.__nome = valor
+        self.__chave = Nome.criaChave(valor)
+
+    @property
+    def chave(self):
+        return self.__chave
 
     @staticmethod
     def criaChave(nome):
